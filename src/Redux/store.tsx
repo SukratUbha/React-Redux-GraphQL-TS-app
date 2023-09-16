2ee13f23bb47b94fe3428e8c9271b57d3b844561
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2023-09-16 15:25:25 +1200

    Added redux provider

---
+++ src/Redux/store.tsx
@@ -0,0 +1,9 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
