15ce782af330ff683a3d1aa17c3cfedf82b1f128
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2023-09-26 21:23:46 +1300

    card image and styling fixes

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,20 +1,31 @@
-//import React from 'react'
-
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { animeType } from './Search';
+import { increment as likesIncrement } from '../features/counter/likesSlice';
+import { useSelector } from 'react-redux';
+type likesType = {
+    likes: { value: number }
+}
 
 export default function Cards(c: animeType) {
+    const { value } = useSelector((state: likesType) => state.likes)
+    console.log(c);
     return (
-            <Card className="card" >
-                <Card.Img variant="top" src={c.coverImage} />
-                <Card.Body>
-                    <Card.Title>{ c.title.english }</Card.Title>
-                    <Card.Text className="cardDescription">
-                        { c.description }
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+        <Card>
+            <div className="card-img">
+                <Card.Img variant="top" src={c.coverImage.medium} />
+            </div>
+            <Card.Body>
+                <Card.Title className="card-title">{c.title.english}</Card.Title>
+                <Card.Text className="cardDescription">
+                    {c.description}
+                </Card.Text>
+            </Card.Body>
+            <div className="btn-card-div">
+                <Button variant="primary" className="btn-card-like" onClick={() => likesIncrement}>Like</Button>
+                <p className="like-counter">{0 + value}</p>
+                <Button variant="primary" className="btn-card-dislike">dislike</Button>
+            </div>
+        </Card>
     )
 }
